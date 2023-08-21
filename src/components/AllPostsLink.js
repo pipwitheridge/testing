72365e55dad617e5e6bbd66391d82fd8631eefaf
pipwitheridge <phillipwitheridge@gmail.com>
2023-08-21 17:05:39 +1000

    Removed window reference

--- src/components/AllPostsLink.js
+++ src/components/AllPostsLink.js
@@ -5,6 +5,6 @@ import Link from 'next/link';
 
 export default function AllPostsLink(props) {
     return(
-      <div><Link href={props.postType} className="smallAnchor">{window.innerWidth>768 ? props.postType==="books" ? "All Book Reviews" : "All Posts" : "All"} {"\u2192"}</Link></div>
+      <div><Link href={props.postType} className="smallAnchor">All {"\u2192"}</Link></div>
     )
   }
