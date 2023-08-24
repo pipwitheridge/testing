9ab923a50ccd29ab55e1ff27a3013e9ed591578b
pipwitheridge <phillipwitheridge@gmail.com>
2023-08-24 21:36:54 +1000

    Removed some files. Added joined up life review.

---
+++ src/components/posts/AJoinedUpLife.js
@@ -0,0 +1,44 @@
+import React from 'react' 
+
+function AJoinedUpLife() {
+    return(
+        <>
+<p>
+This year at college one of our assessments involved explaining Jesus’ ethical reasoning in Matthew 16:24-28, where he calls disciples to “deny yourself, take up your cross and follow me.” 
+</p>
+<p>
+How would you explain why it’s right to follow Jesus? There are all sorts of options, such as:
+<ul>
+    <li>“well if Jesus is God, of course you have to follow him!”</li>
+    <li>“if you don’t follow Jesus, the bible promises a pretty grim afterlife.”</li>
+    <li>“if someone wants to follow Jesus, that’s fine—but it might not be for everyone.”</li>
+    <li>“the bible says so, and that’s the end of the matter.”</li>
+    <li>“Jesus is worth following because his teachings are incredible.”</li>
+</ul>
+</p>
+<p>
+Lots of options, some better than others.
+</p>
+<p>
+One book that was helpful in thinking through Jesus’ ethical reasoning and how it affects Christian life was <i>A Joined-up Life</i> by Andrew Cameron. It gives a clear overview of different ethical frameworks and how they operate, and it gives some compelling conclusions about how ethics might be useful for Christians.
+</p>
+<p>
+Ultimately Cameron argues that ethics is useful because it forces us to consider how Jesus lived and how we might follow him: 
+</p>
+<p>
+<em>“Jesus challenges abstract talk about ethics by his person, so that according to the Bible the main question becomes not ‘Am I living ethically?’, but ‘How am I responding to Jesus Christ?’”</em>
+</p>
+<p>
+Cameron acknowledges and tackles the complexity of ethics in the modern world. He tackles issues like sustainability, work, sex, and bioethics. I thought the section on work was particularly thought-provoking. He explores how we might view our work through the lens of Christian ethics and he offers advice on how to assess if work is ‘good’ or ‘bad’. For example, he says: 
+</p>
+<p>
+<em>“Work is bad if it so persistently disorders creation as to generate new work for whoever has to clean up the mess … Work is bad if it damages or destroys communities … such as the destruction of people or families or habitats … or by such enormous time demands that communities other than the workplace become impossible … Work is bad when it’s nothing but toilsome and laborious. When workers can find no way that their work assists a community to care, they’ve found a sad truth about their work.”</em>
+</p>
+<p>
+Besides walking away with some big new words like ‘deontology’ and ‘teleology’, I found the book helpful because it got me thinking about different kinds of reasons for why following Jesus is ‘good’. In a culture that says following Jesus is problematic, it’s helpful to reflect deeply on the ‘why’ of being a Christian. I’d recommend this book to anyone wanting to do that!
+</p>
+</>
+    )
+}
+
+export default AJoinedUpLife;
