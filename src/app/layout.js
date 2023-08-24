e26365d59d985f00b7ff4e7159be6d1324ed4821
pipwitheridge <phillipwitheridge@gmail.com>
2023-08-24 15:58:46 +1000

    Removed 'wideImage' and added Google SEO meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="Rw0Vf2ENY8WpRhFAMnSeXewDqp4c9S2nZbc6LbqYyJc" />
+      </head>
       <body className={inter.className}> 
       <div className="container col-xl-7 col-lg-8 col md-12 pb-5" style={{marginTop: 20}}>
         {children}
