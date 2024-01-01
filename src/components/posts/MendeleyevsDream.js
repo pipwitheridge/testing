df3d5835a60cbd83a2c469591cb99e188e8fb3ac
pipwitheridge <phillipwitheridge@gmail.com>
2024-01-01 15:51:27 +1100

    Edited mendeleyev review

--- src/components/posts/MendeleyevsDream.js
+++ src/components/posts/MendeleyevsDream.js
@@ -7,7 +7,7 @@ function MendeleyevsDream() {
         In this book, Paul Strathern unravels the history of chemistry, focusing on the quest for elements and the pivotal role of 19th-century Russian scientist Dmitri Mendeleyev. After a restless period of solitude trying to uncover a system that made sense of the elements, Mendeleyev finally fell asleep at his desk. He had a dream which would shape the future of chemistry. When he awoke he wrote down what he had dreamed about—the periodic table. 
         </p>
         <p>
-        Strathern is a great story-teller of history. The book isn't primarily about Mendeleyev—most of it tells the history of chemistry in such a way that lays the groundwork for the final two chapters, which are about Mendeleyev. If lessons are to be learned from this history it is that what we know today as modern chemistry came from a long process of trial, much error, hard work, argument, experiment and discovery. This things which make up the history of chemistry also make up the lives of some of its great dreamers.
+        Strathern is a great story-teller of history. He makes clear that modern chemistry came from a long process of trial, much error, hard work, argument, experiment and discovery. These things make up the history of chemistry; they also make up the lives of some of its great dreamers.
         </p>
         <p>
         I'd recommend this to anyone wanting a readable sketch of the history of chemistry for a popular audience. 
