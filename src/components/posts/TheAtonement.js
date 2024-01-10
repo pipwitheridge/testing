4302b78ccb595767fb124298e63aa0cd1f5d335b
pipwitheridge <phillipwitheridge@gmail.com>
2024-01-10 16:07:47 +1100

    new post on The Atonement

---
+++ src/components/posts/TheAtonement.js
@@ -0,0 +1,19 @@
+import React from 'react' 
+
+function TheAtonement() {
+    return(
+        <>
+        <p>
+        Many would say that the cross is at the very centre of Christianity. If you're a Christian wanting to deepen in your faith, or if you're just someone wanting to explore why Jesus' death is a centrepiece of the Christian faith, then this book is gold.
+        </p>
+        <p>
+        Leon Morris, an Australian, was a New Testament scholar and principal of Ridley College in Melbourne. His book on Atonement is structured around exploring key concepts we sometimes neglect: covenant, sacrifice, the Day of Atonement, the Passover, redemption, reconciliation, propitiation, and justification. In each chapter he explores what is said by the Old Testament, Rabbinic literature, and the New Testament, and presents various points of views while arguing for his own views quite persuasively. I'll return to this book when I next preach on any of these topics. 
+        </p>
+        <p>
+        The book can be dense but nonetheless is readable for a motivated student of the cross. Would recommend.
+        </p>
+        </>
+    )
+}
+
+export default TheAtonement;
